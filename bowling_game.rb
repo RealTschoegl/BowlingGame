c9fc79b5d822f49046a3000b164aff4023ae69db
MakerSquare Student <students@themakersquare.com>
2013-08-01 10:04:14 -0500

    Added scoring.

--- bowling_game.rb
+++ bowling_game.rb
@@ -3,12 +3,33 @@ class BowlingGame
     @rolls = []
   end
 
+  # Record a roll in the game.
+  #
+  # pins - The Integer number of pins knocked down in this roll.
+  #
+  # Returns nothing.
   def roll(pins)
-
+    @rolls.push(pins)
   end
 
   def score
-    0
+    total_score  = 0
+    current_roll = 0
+
+    while current_roll < @rolls.size - 1
+      roll      = @rolls[current_roll]
+      next_roll = @rolls[current_roll + 1]
+
+      if roll + next_roll == 10
+        total_score += 10 + @rolls[current_roll + 2]
+      else
+        total_score += roll + next_roll
+      end
+
+      current_roll += 2
+    end
+
+    return total_score
   end
 
 end
--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -13,11 +13,19 @@ describe BowlingGame do
     end
 
     context 'when one pin is knocked down on each roll' do
-      context 'when only one pin is knocked down with each roll' do
-        it 'scores the game as 20' do
-          20.times { game.roll(1) }
-          expect(game.score).to eq(20)
-        end
+      it 'scores the game as 20' do
+        20.times { game.roll(1) }
+        expect(game.score).to eq(20)
+      end
+    end
+
+
+    context 'when a spare is thrown' do
+      it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
+        3.times  { game.roll(5) }
+        17.times { game.roll(0) }
+
+        expect(game.score).to eq(20)
       end
     end
   end
